61577effbe50d98ccb11b71ab7dd070139f0043c
venkatarohan <venkatarohank@gmail.com>
2023-11-26 19:28:22 +0530

    working

--- copy/copy.js
+++ copy/copy.js
@@ -4,11 +4,14 @@ import { Peer } from "peerjs";
 import CustomImage from './img.png'
 import ReactPlayer from 'react-player'
 
-
+// const myPeer = new Peer(undefined, {
+//   host: '/',
+//   port: '3001'
+// })
 const socket = io('https://webrtc-backend-hq7d.onrender.com')
 //const socket = io('http://localhost:8000')
 const myPeer = new Peer()
-
+//const screensharePeer = new Peer()
 function App() {
   const [arr, setarr] = useState([])
   const [streamm, setStream] = useState([])
@@ -19,9 +22,17 @@ function App() {
   const [audio, setAudio] = useState(true)
   const [screenShare, setscreenShare] = useState()
   const [calls, setCalls] = useState([])
-  const [scpeer, setscpeer] = useState()
+  const [scpeer,setscpeer]=useState()
   useEffect(() => {
+    // Create a WebSocket connection to the server
+    // const socket = io('http://localhost:8000')//io('https://webrtc-backend-bfblx66zz-venkatarohank-gmailcom.vercel.app'); // Replace with your server's address
 
+    //  navigator.mediaDevices.getDisplayMedia({
+    //   video: {
+    //       cursor: "always"
+    //   },
+    //   audio: false
+    ////console.log(socket);
     navigator.mediaDevices.getUserMedia({
       video: true,
       audio: true
@@ -40,16 +51,50 @@ function App() {
             console.log(screenShare);
             setscreenShare(userVideoStream)
             console.log(userVideoStream);
-
+          //  setscreenShare()
+           // setscreenShare((prev)=>{
+        //       console.log(prev);
+        //       if(prev){
+        //       //   const v = prev[0].props.stream.getTracks().find((ele) => ele.kind === 'video')
+        //       //   //console.log(prev[0].props.stream);
+        //       //  // console.log(v);
+        //       // //  prev[0].props.stream.removeTrack(v)
+        //       //   prev[0].props.stream.addTrack(userVideoStream.getTracks().find((ele) => ele.kind === 'video'))
+        //       //   return prev
+        //       // return [...prev,<AddVideoStream userid={call.peer} playing={true} own={false} stream={userVideoStream}/>]
+        //       return [ <div className="video-window"><ReactPlayer
+        //   playsinline // very very imp prop
+        //   pip={false}
+        //   //light={CustomImage}
+        //   controls={false}
+        //   // playIcon={false}
+        //   muted={true}//own
+        //   playing={true}
+        //   //
+        //   url={userVideoStream}//{"https://www.youtube.com/watch?v=DvlyzDZDEq4"}//{videoStream}
+        //   //
+        //   height={"300px"}
+        //   width={"300px"}
+        //   className="video-window"
+        //   onError={(err) => {
+        //     ////console.log(err, "participant video error");
+        //   }}
+        // />
+        //   <p>{call.peer}</p>
+        // </div>]
+        //       }
+             // return [<AddVideoStream userid={call.peer} playing={true} own={false} stream={userVideoStream}/>]
+            // })
             console.log(userVideoStream.getTracks());
             console.log(userVideoStream);
             console.log("scerwafsdcsdfasdfasfasdf");
           } else {
             setarr((prev) => [...prev, <AddVideoStream userid={call.peer} playing={true} own={false} stream={userVideoStream} />])
-          }
+         }
 
           call.on('close', (val) => {
-
+            ////console.log("closse");
+            ////console.log(val);
           })
 
         })
@@ -57,7 +102,7 @@ function App() {
 
       socket.on('user-connected', userId => {
         var call = connectToNewUser(userId, stream)
-
+        // setCalls((prev) => [...prev, call])
       })
     })
 
@@ -77,18 +122,27 @@ function App() {
 
 
     socket.on("newMsg", (val, soc_id) => {
+      ////console.log(val);
       setMsgArr((prev) => [...prev, <div className="chat-message"><p className='normal-text'>{val}</p> <p className='subscript-text'>{soc_id}</p></div>])
     })
 
 
+    // socket.on('disconnect', () => {
+    //   ////console.log('Disconnected from the WebSocket server');
+    // });
+
     // Clean up the WebSocket connection when the component unmounts
 
     socket.on('server_stop_screenshare', () => {
-      setscreenShare()
-    })
+          setscreenShare()
+      })
 
     socket.on('server_screenshare', usrId => {
-
+      //console.log(usrId);
+      // navigator.mediaDevices.getUserMedia({
+      //   video: false,
+      //   audio: true
+      // }).then(str => {
       console.log(usrId);
       var pp
       const call = myPeer.call(usrId, new MediaStream())
@@ -96,11 +150,13 @@ function App() {
       call.on('stream', userVideoStream => {
         console.log("usr call");
         setarr((prev) => [...prev, <AddVideoStream userid={usrId} playing={true} own={false} stream={userVideoStream} screenShare={true} />])
-
+        // })
 
       })
 
 
+      // setCalls((prev) => [...prev, call])
+      ////console.log(calls);
     })
 
     return () => {
@@ -116,17 +172,61 @@ function App() {
       return temp
     })
   })
+  //   screensharePeer.on('call', call => {
+  //     call.answer(strea)
+  //     console.log("got call");
+  //     call.once('stream', (userVideoStream) => {
+  //       console.log("un stream");
+  //       //console.log(userVideoStream);
+  //       //console.log(userVideoStream.getTracks());
+  //       ////console.log(userVideoStream);
+  //       ////console.log(call.peer);
+  //      // setarr((prev) => [...prev, <AddVideoStream userid={call.peer} playing={true} own={false} stream={userVideoStream} />])
+  //       //console.log(call.peerConnection.getSenders());
+  //       call.on('close', (val) => {
+  //         ////console.log("closse");
+  //         ////console.log(val);
+  //       })
 
+  //     })
+  //     //console.log(stream);
+  // })
+  // socket.once('server_screenshare', usrId => {
+  //   //console.log(usrId);
+  //   navigator.mediaDevices.getUserMedia({
+  //     video: true,
+  //     audio: true
+  //   }).then(str => {
+  //     //console.log(usrId);
+  //     const call = myPeer.call(usrId,str)
+  //     //console.log(call);
+  //     call.once('stream', userVideoStream => {
+  //       //console.log(userVideoStream);
+  //       //console.log(userVideoStream.getTracks());
+  //     // setscreenShare(userVideoStream)
+  //        setarr((prev) => [...prev, <AddVideoStream userid={usrId} playing={true} own={false} stream={userVideoStream} screenShare={true} />])
+  //   })
+
+  //   })
+
+
+  //  // setCalls((prev) => [...prev, call])
+  //   ////console.log(calls);
+  // })
 
   socket.once("server_track_toggle", (usrid, soc_id, isvideoplaying, trackType) => {
-
+    //console.log("sdfasdfd   " + isvideoplaying);
     setarr((prev) => {
       const temp = prev.map((val) => {
         if (val.props.userid === usrid) {
+          // Replace the component with the new key
+          //console.log(val.props.stream.getTracks());
+          //val.props.stream.getTracks()
           return <AddVideoStream userid={usrid} playing={isvideoplaying} stream={val.props.stream} own={false} />
         }
         return val
       });
+      //console.log(temp);
       return temp
     })
   })
@@ -169,13 +269,35 @@ function App() {
     navigator.mediaDevices.getDisplayMedia({
       video: true//{ cursor: "always" }
     }).then((screenstream) => {
-      // if(Object.keys(screensharePeer.connections).length==0){
-      Object.keys(myPeer.connections).forEach((ele) => {
-        screensharePeer.call(ele, screenstream, { metadata: "screenshare" })
-      })
-      screensharePeer.call(myPeer.id, screenstream, { metadata: "screenshare" })
+     // if(Object.keys(screensharePeer.connections).length==0){
+        Object.keys(myPeer.connections).forEach((ele) => {
+          screensharePeer.call(ele, screenstream, { metadata: "screenshare" })
+        })
+        screensharePeer.call(myPeer.id, screenstream, { metadata: "screenshare" })
+      // }else{
+      //   Object.keys(screensharePeer.connections).forEach((ele) => {
+      //   const v = screenstream.getTracks().find((ele) => ele.kind === 'video')
+      //   screensharePeer.connections[ele][0].peerConnection.getSenders()[0].replaceTrack(v)
+      //   })
+      // }
+      
+      console.log(screensharePeer);
+      // screensharePeer.on('call', call => {
+      //     call.answer(strea)
+      //     console.log("got call");
+      //     call.once('stream', (userVideoStream) => {
+      //       console.log("un stream");
 
+      //       call.on('close', (val) => {
+      //         ////console.log("closse");
+      //         ////console.log(val);
+      //       })
 
+      //     })
+      //     //console.log(stream);
+      // })
+      //  socket.emit('screenshare',screensharePeer.id)
+      console.log(screensharePeer.id);
     })
 
   }
@@ -189,7 +311,7 @@ function App() {
   return (
     <div>
       {screenShare && <div >
-        <div className="screen-share-container"><ReactPlayer
+      <div className="screen-share-container"><ReactPlayer
           playsinline // very very imp prop
           pip={false}
           //light={CustomImage}
@@ -202,25 +324,35 @@ function App() {
           //
           height={"70%"}
           width={"70%"}
-          // className="video-window"
+         // className="video-window"
           onError={(err) => {
             ////console.log(err, "participant video error");
           }}
         />
-          <p>{scpeer ? scpeer.id : "12"}</p>
-          {scpeer && <button onClick={() => {
-            Object.keys(scpeer.connections).forEach((ele) => {
-              scpeer.connections[ele][0].peerConnection.getSenders()[0].track.stop();
-              // screensharePeer.connections[ele][0].peerConnection.getSenders()[1].track.stop();
-            })
-            socket.emit("stop_screenshare")
-            setscreenShare()
-            setscpeer()
-
-          }}>Close</button>}
+          <p>{scpeer?scpeer.id:"12"}</p>
+          { scpeer&& <button onClick={() => {
+          // Object.keys(screensharePeer.connections).forEach((ele)=>{
+          //  // if(screensharePeer.connections[ele]){
+          //   //  screensharePeer.connections[ele][0].close()
+          //     console.log(screensharePeer.connections[ele][0].dataChannel.close());
+          //   //}
+          // })
+          // console.log(screensharePeer);
+          Object.keys(scpeer.connections).forEach((ele)=>{
+            scpeer.connections[ele][0].peerConnection.getSenders()[0].track.stop();
+            // screensharePeer.connections[ele][0].peerConnection.getSenders()[1].track.stop();
+          })
+          socket.emit("stop_screenshare")
+          setscreenShare()
+          setscpeer()
+          //screensharePeer.connections={}
+          // console.log(screensharePeer._cleanup());
+          // setscreenShare()
+          // console.log(screensharePeer);
+        }}>Close</button>}
         </div>
         {/* <AddVideoStream userid={scpeer?scpeer.id:"12"} playing={true} stream={screenShare} own={false} /> */}
-        {/* <ReactPlayer
+      {/* <ReactPlayer
           playsinline // very very imp prop
           pip={false}
           //light={CustomImage}
@@ -238,9 +370,9 @@ function App() {
             ////console.log(err, "participant video error");
           }}
         /> */}
-
+       
         {/* <AddVideoStream playing={true} own={false} stream={screenShare} /> */}
-
+      
       </div>}
       <div className="video-conference-container">
         <div className="video-grid">
@@ -248,7 +380,43 @@ function App() {
           {/* Add more remote video windows as needed */}
         </div>
         <div className="controls">
-
+          {/* Add controls like mute, video on/off, screen sharing, etc. */}
+          {/* <button onClick={() => {
+          ////console.log(streamm);
+          if (streamm) {
+            if (video) {
+              const cam_per=navigator.permissions.query({name:"camera"})
+              //console.log(navigator.permissions)
+            
+              const v_track = streamm.getTracks().find((ele) => ele.kind === 'video')
+              v_track.stop()
+            //  v_track.enabled=false
+             //v_track.muted=true
+              //console.log(v_track);
+             // streamm.removeTrack(v_track)
+            } else {
+              navigator.mediaDevices.getUserMedia({
+                video: true,
+              }).then(stream => {
+                setStream((prev) => {
+                  ////console.log(stream.getTracks().find((ele)=>ele.kind==='video'));
+                  //console.log(stream.getTracks());
+                  const v_track = streamm.getTracks().find((ele) => ele.kind === 'video')
+                  prev.removeTrack(v_track)
+                    prev.addTrack(stream.getTracks()[0])
+                    
+                    //console.log(prev.getTracks());
+                  return prev
+                })
+              })
+              const v_track = streamm.getTracks().find((ele) => ele.kind === 'video')
+              v_track.enabled=true
+              //console.log(v_track);
+            }
+            socket.emit("video_toggle", myPeer.id, socket.id, !video)
+            setVideo(!video)
+          }
+        }}>{video ? "Video off" : "Video on"}</button> */}
           <button onClick={() => {
 
             if (streamm) {
@@ -272,7 +440,7 @@ function App() {
                   video: true,
                 }).then((stream) => {
                   const v = stream.getTracks().find((ele) => ele.kind === 'video')
-                  if (v) {
+                  if(v){
                     myPeer.connections[ele][0].peerConnection.getSenders()[1].replaceTrack(v)
                     setStream((prev) => {
                       const v_track = prev.getTracks().find((ele) => ele.kind === 'video')
@@ -280,10 +448,10 @@ function App() {
                       prev.addTrack(v)
                       return prev
                     })
-                  } else {
+                  }else{
 
                   }
-
+                 
                 })
               }
               //  //console.log(ReactPlayer);
@@ -293,6 +461,13 @@ function App() {
           }}>{video ? "Video off" : "Video on"}</button>
           <button onClick={openPopup}>Chat</button>
           <button onClick={openScreenSharePopup}>Screen Share</button>
+          {/* <button onClick={() => {
+            ////console.log(calls);
+            calls.forEach((call) => {
+              call.close()
+            })
+
+          }}>leave</button> */}
           {showPopup && (
             <div className="popup">
               <div className="popup-content">
@@ -327,3 +502,10 @@ function App() {
 
 export default App;
 
+
+// {screenShare&& <div className="screen-share-container">
+// <h1 className="screen-share-title">Screen Sharing App</h1>
+// <button className="screen-share-button">Start Sharing</button>
+// {/* Display the shared screen/video */}
+// <AddVideoStream  playing={true} own={false} stream={screenShare}/>
+// </div>}
